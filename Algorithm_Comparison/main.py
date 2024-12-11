1008e5e651000cd603717e297d61c8c645d322e9
Andromius <miniswiczmartin@gmail.com>
2024-12-11 13:36:32 +0100

    Added algorithm comparison

---
+++ Algorithm_Comparison/main.py
@@ -0,0 +1,48 @@
+from Particle_Swarm_Optimization.main import do_PSO
+from Firefly_Algorithm.main import do_FA
+from Teaching_Learning_Based_Optimization.main import do_TLBO
+from SOMA_AllToOne.main import do_SOMA
+from Differential_Evolution.main import do_DE
+import pandas as pd
+import Differential_Evolution.constants as cons
+import CV01.animation_students as anim
+
+
+values = []
+dimensions = 30
+pop_size = 30
+max_ofe = 3000
+num_experiments = 30
+objective_function = anim.schwefel
+bounds = cons.SCHWEFEL
+
+for algorithm in [do_PSO, do_FA, do_TLBO, do_SOMA, do_DE]:
+    results = []
+    for experiment in range(num_experiments):
+        results.append(algorithm(dimensions, pop_size, 9999, max_ofe, objective_function, bounds))
+    values.append(results)
+    print("Next..")
+
+data = {
+    'PSO': values[0],
+    'FA': values[1],
+    'TLBO': values[2],
+    'SOMA': values[3],
+    'DE': values[4]
+}
+
+df = pd.DataFrame(data)
+
+means = df.mean()
+std_devs = df.std()
+
+df.loc['Mean'] = means
+df.loc['Std_Dev'] = std_devs
+
+
+excel_file = "algo_comp_output.xlsx"
+df.to_excel(excel_file, index=True)
+
+# Display the DataFrame
+print(df)
+
